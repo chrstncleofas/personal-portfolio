4cfc49d48a4e1487d9ead2ed3ff55f07bfdd527d
chrstn <cleofastiano27@gmail.com>
2020-06-20 16:40:28 -0700

    Portfolio Updates

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// Para sa Smooth Scrolling
+$('#header a').on('click', function(event) {
+    if (this.hash !== '') {
+      event.preventDefault();
+  
+      const hash = this.hash;
+  
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top -50
+      },
+      800
+    );
+  }
+});
